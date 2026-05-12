882260a62eb2de6284eec854c336dd6d187f9e99
AntonioCastanoBarril <acb0076@alu.medac.es>
2026-05-12 19:21:21 +0200

    segundo commit

---
+++ src/GestorTareas.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorTareas {
+    private List<Tarea> tareas;
+    private int siguienteId;
+
+    public GestorTareas(){
+        this.tareas = new ArrayList<>();
+        this.siguienteId = 1;
+    }
+
+    public void agregarTarea(String titulo, String descripcion){
+        Tarea nuevaTarea = new Tarea(siguienteId, titulo, descripcion, false);        
+        tareas.add(nuevaTarea);
+        siguienteId++;
+    }
+
+    public List<Tarea> obtenTareas(){
+        return tareas;
+    }
+
+    public Tarea buscarTareaPorId(int id){
+        for(Tarea tarea : tareas){
+            if (tarea.getId()==id) {
+                return tarea;
+            }
+        }
+        return null;
+    }
+
+    public boolean completarTarea(int id){
+        Tarea tarea = buscarTareaPorId(id);
+        if (tarea!=null) {
+            tarea.marcarComoCompletada();
+            return true;
+        }
+        return false;
+    }
+}
