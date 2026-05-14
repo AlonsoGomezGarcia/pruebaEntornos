d0f8c15fa434fcd9bf17fbc97bbf9a41646c7182
AlonsoGomezGarcia <agg0185@alu.medac.es>
2026-05-14 18:46:22 +0200

    cambio en el main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // 1. Creamos el motor de datos
+        GestorTareas gestorTareas = new GestorTareas();
         
+        // 2. Creamos el motor de edición pasándole el gestor de tareas
+        GestorEdicion gestorEdicion = new GestorEdicion(gestorTareas);
+        
+        // 3. Inicializamos el menú con ambos gestores
+        Menu menu = new Menu(gestorTareas, gestorEdicion);
+
+        // 4. Arrancamos la aplicación
+        menu.iniciar();
     }
 }
